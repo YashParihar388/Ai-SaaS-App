6cdfd82917feab02c66eb7ca0ed420b9fdc4fdcf
YashParihar388 <yparihar8085@gmail.com>
2025-09-03 11:42:57 +0530

    home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import NavBar from '../components/NavBar';
+import Hero from '../components/Hero';
+import Aitools from '../components/Aitools';
+import Testimonial from '../components/Testimonial';
+import Plan from '../components/Plan'
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <>
         <NavBar />
+        <Hero />
+        <Aitools />
+        <Testimonial />
+        <Plan />
+        <Footer />
         
       
     </>
